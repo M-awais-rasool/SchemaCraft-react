f7a0b3b8d775831ce8f4aaa61e31107255f15de5
M-awais-rasool <ar30781871@gmail.com>
2025-09-06 13:57:38 +0500

    style: remove trailing whitespace in api.ts for cleaner code

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
+ 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('auth_token');
